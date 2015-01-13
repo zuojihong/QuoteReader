d7f6c729350aa9d3afccc9f2a172fd2f2dda6db3
jihongzuo <zuojihong@hotmail.com>
2015-01-14 00:02:49 +0800

    fix issue that reader cannot start automatically

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ var http = require('http');
 var quoteReader = require('./quoteReader');
 
 var port = process.env.port || 8886;
+
+setInterval(quoteReader.fetchStockData, 5000);
+
 http.createServer(function (req, res) {
-    //res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //res.end('QuoteReader Started\n');
-
-    setInterval(quoteReader.fetchStockData, 5000);
-    /*
-    var newDate = new Date();
-    newDate.setHours(9, 0, 0, 0);
-    console.log('\nnew date: ' + newDate.toString());
-    console.log('\nnew date in milli: ' + newDate.getTime());
-
-    var nDate = new Date();
-    nDate.setTime(1421055827000);
-    console.log('\nanother date: ' + nDate.toString());
-    */
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('QuoteReader Started\n');
+    
 }).listen(port);
